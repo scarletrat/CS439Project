17f7bcb4ac883ba3ce8efcc0a3143a0447a5ff05
Gordon Lin <glin3132@gmail.com>
2026-05-03 17:25:22 -0400

    add dataset script

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+from ucimlrepo import fetch_ucirepo
+
+# Example dataset (you can change ID later)
+student_performance = fetch_ucirepo(id=320) 
+
+X = student_performance.data.features
+y = student_performance.data.targets
+
+print(X.head())
+print(y.head())
+  
+# metadata 
+print(student_performance.metadata) 
+  
+# variable information 
+print(student_performance.variables) 
